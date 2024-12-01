987513c5515b4e0f09a27b13cd341d2417b825c5
GreenFrog-fs <aaaartemaa@gmail.com>
2024-12-01 16:30:10 +0700

    featire: new proxy

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ const bot = new Bot(telegramToken);
 async function chatWithGpt(prompt) {
   try {
     const response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
+      "https://api.proxyapi.ru/openai/v1/chat/completions",
       {
-        model: "gpt-3.5-turbo",
+        model: "gpt-4-turbo",
         messages: [{ role: "user", content: prompt }],
-        max_tokens: 150,
-        temperature: 0.7,
       },
       {
         headers: {
